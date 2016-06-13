faa654f0c30e1c7b37ebf058c253e2e58c830e48
Michael Fossel <mikefossel@gmail.com>
2016-06-12 17:57:57 -0700

    Update lease.create.ctlr.js

--- PropertyManager.Web/js/app/lease/lease.create.ctlr.js
+++ PropertyManager.Web/js/app/lease/lease.create.ctlr.js
@@ -1,8 +1,10 @@
 ﻿angular.module('app').controller('LeaseCreateController', function ($scope, $stateParams, LeaseResource, TenantResource, PropertyResource) {
+    
     $scope.lease = {};
     $scope.newLease = {};
     $scope.tenants = TenantResource.query();
     $scope.properties = PropertyResource.query();
+    
     $scope.createLease = function () {
         LeaseResource.save($scope.newLease, function () {
             $scope.newLease = {};
